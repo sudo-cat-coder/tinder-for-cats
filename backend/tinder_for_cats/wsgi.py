f80bd121b3ebd3cf59a0f19aaf9eb894619d5f7f
cat <hesam123409@gmail.com>
2026-08-15 20:29:08 +0330

    start again with venv add nextjs

---
+++ backend/tinder_for_cats/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for tinder_for_cats project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tinder_for_cats.settings')
+
+application = get_wsgi_application()
